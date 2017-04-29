b0834e0a063dc7d2ee1cd2a02e2a2e03d81a2849
IgorPerikov <perikov.igor@gmail.com>
2017-04-29 22:24:51 +0300

    define cql keyspace and init udt

--- common/src/main/java/org/clayman/scoring/common/domain/MatchInfo.java
+++ common/src/main/java/org/clayman/scoring/common/domain/MatchInfo.java
@@ -2,14 +2,18 @@ package org.clayman.scoring.common.domain;
 
 import lombok.Data;
 
-import java.time.ZonedDateTime;
+import java.time.Instant;
 import java.util.Map;
 import java.util.UUID;
 
 @Data
 public class MatchInfo {
     private UUID id;
+
     private Map<UUID, PlayerStats> playerStats;
+
+    private Instant startTime;
+    private Instant endTime;
     private MatchType matchType;
-    private ZonedDateTime time;
+    private String mapName;
 }
---
+++ common/src/main/java/org/clayman/scoring/common/domain/Region.java
@@ -0,0 +1,7 @@
+package org.clayman.scoring.common.domain;
+
+public enum Region {
+    EUROPE,
+    ASIA,
+    AMERICA
+}
